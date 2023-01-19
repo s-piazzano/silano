d1ca4a4ba739368dda09ffb4e2147ec0109120b6
stefano <stefano@MacBook-Pro-di-stefano.local>
2023-01-19 10:19:27 +0100

    add base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/silano',
   images: {
     domains: ["silano-3r.fra1.digitaloceanspaces.com"],
   },
