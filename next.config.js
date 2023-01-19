4308e116ef308949dd91dfa1bb88987087b38b15
stefano <stefano@MacBook-Pro-di-stefano.local>
2023-01-19 16:53:59 +0100

    change basedir

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/silano',
   images: {
     domains: ["silano-3r.fra1.digitaloceanspaces.com"],
   },
