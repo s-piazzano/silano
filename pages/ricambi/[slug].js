6f686711cfff7ec6b5c7bcaee2573ed8393827b7
Stefano <stefano@MacBook-Pro-di-Stefano.local>
2023-03-14 14:52:13 +0100

    change slug in ricambi

--- pages/ricambi/[slug].js
+++ pages/ricambi/[slug].js
@@ -1,19 +1,3 @@
 export default function Product() {
   return <div>product</div>;
 }
-
-export async function getStaticPaths() {
-  const pages = await getAllPages();
-
-  // Automatic generation of paths
-  const slugs = pages.map(
-    (x) =>
-      new Object({
-        params: x.attributes,
-      })
-  );
-  return {
-    paths: slugs,
-    fallback: false, // can also be true or 'blocking'
-  };
-}
