e6c3d596c5193569f0805d47b22e5121c8564853
Stefano <stefano@MacBook-Pro-di-Stefano.local>
2023-03-28 17:47:40 +0200

    change strategy of api bakeka

--- pages/bakeca.xml.js
+++ pages/bakeca.xml.js
@@ -12,7 +12,17 @@ function generateBakecaXml(items) {
     } ${item.attributes.compatibilities
       .map(
         (comp) =>
-          `${comp.make.data.attributes.name} ${comp.model.data.attributes.name}`
+          `${comp.make.data ? comp.make.data.attributes.name : ""} ${
+            comp.model.data ? comp.model.data.attributes.name : ""
+          } ${
+            comp.engine_capacity.data && comp.engine_capacity.data.id != 5
+              ? comp.engine_capacity.data.attributes.capacity
+              : ""
+          } ${
+            comp.fuel_system.data && comp.fuel_system.data.id != 8
+              ? comp.fuel_system.data.attributes.name
+              : ""
+          }`
       )
       .join(" / ")} ${
       item.attributes.OE ? item.attributes.OE.toUpperCase() : ""
@@ -21,6 +31,8 @@ function generateBakecaXml(items) {
 
   function generateDescription(item) {
     return `
+      Il prezzo di 1€ è indicativo - Contattaci per conoscere la quotazione effettiva.
+
       Autodemolizione specializzata nella vendita ricambi usati. 
       Offriamo come ricambio usato funzionante ${
         item.attributes.sub_category.data[0].attributes.name
@@ -28,13 +40,15 @@ function generateBakecaXml(items) {
       ${item.attributes.compatibilities
         .map(
           (comp) =>
-            `- ${comp.make.data.attributes.name} ${comp.model.data.attributes.name}`
+            `- ${comp.make.data ? comp.make.data.attributes.name : ""} ${
+              comp.model.data ? comp.model.data.attributes.name : ""
+            }`
         )
         .join("")}
 
       Disponiamo di ricambi per carrozzeria, meccanica, parti elettriche ed elettroniche, selleria...
       I ricambi sono accuratamente smontati e catalogati in magazzino da personale qualificato.
-      ƒ
+      
       Rispondiamo quotidianamente alle vostre e-mail.
  
       Possibilità di spedizione in tutta Italia
@@ -47,113 +61,115 @@ function generateBakecaXml(items) {
 
   const xmlFile = `
     <ITEMLIST>
-      ${items
-        .map(
-          (item) => `<ITEM ID="${
+    ${items
+      .map((item) => {
+        if (item.attributes.images && item.attributes.images.data.length) {
+          return `<ITEM ID="${
             item.id
           }" CITYID="${idCity}" CATEGORYID="${idCat}" LASTUPDATE="${new Date(
             item.attributes.updatedAt
           ).toUTCString()}" EXPIRED="0">
-          <ISTAT>${istat}</ISTAT>
-          <TITLE><![CDATA[${generateTitle(item)}]]></TITLE>
-          <TEXT><![CDATA[${generateDescription(item)}]]></TEXT>
-          <EMAIL>${email}</EMAIL>
-          <ATTRIBUTELIST>
-            <ATTRIBUTE>
-              <ATTRID>4</ATTRID>
-              <ATTRNAME><![CDATA[Azienda]]></ATTRNAME>
-              <ATTRVAL><![CDATA[]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>indirizzoagenzia</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[Cascina Fiorina]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>ncivicoagenzia</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[SNC]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>comuneagenzia</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[Tronzano Vercellese]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>capagenzia</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[13049]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>pivaagenzia</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[02397390028]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>contattotelefonico</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[+390161930380]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>contattotelefonico2</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[+393929898074]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>logoagenzia</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[https://silano-3r.fra1.digitaloceanspaces.com/3r/96a20c88b9d573b3f830834a54ad3f79.jpg?updated_at=2023-01-20T15:37:16.481Z]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>linkagenzia</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[www.silanosrl.it]]></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>tipocomprovendo</ATTRID>
-              <ATTRNAME><![CDATA[2190]]></ATTRNAME>
-              <ATTRVAL></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>tiporicambiaccessori</ATTRID>
-              <ATTRNAME><![CDATA[10]]></ATTRNAME>
-              <ATTRVAL></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>condizionericambi</ATTRID>
-              <ATTRNAME><![CDATA[2738]]></ATTRNAME>
-              <ATTRVAL></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>tipocomprovendo</ATTRID>
-              <ATTRNAME><![CDATA[2190]]></ATTRNAME>
-              <ATTRVAL></ATTRVAL>
-            </ATTRIBUTE>
-            <ATTRIBUTE>
-              <ATTRID>prezzo</ATTRID>
-              <ATTRNAME></ATTRNAME>
-              <ATTRVAL><![CDATA[${generatePrice(item)}]]></ATTRVAL>
-            </ATTRIBUTE>
-            
-            </ATTRIBUTELIST>
-
-            
-            <IMAGELIST>
-
-            ${
-              item.attributes.images && item.attributes.images.data.length
-                ? item.attributes.images.data
-                    .map((x) => `<IMGFILE>${x.attributes.url}</IMGFILE>`)
-                    .join("")
-                : ""
-            }
-             
-            </IMAGELIST>
-
-            
-      </ITEM>`
-        )
-        .join("")}
+            <ISTAT>${istat}</ISTAT>
+            <TITLE><![CDATA[${generateTitle(item)}]]></TITLE>
+            <TEXT><![CDATA[${generateDescription(item)}]]></TEXT>
+            <EMAIL>${email}</EMAIL>
+            <ATTRIBUTELIST>
+              <ATTRIBUTE>
+                <ATTRID>4</ATTRID>
+                <ATTRNAME><![CDATA[Azienda]]></ATTRNAME>
+                <ATTRVAL><![CDATA[]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>indirizzoagenzia</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[Cascina Fiorina]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>ncivicoagenzia</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[SNC]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>comuneagenzia</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[Tronzano Vercellese]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>capagenzia</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[13049]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>pivaagenzia</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[02397390028]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>contattotelefonico</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[+390161930380]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>contattotelefonico2</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[+393929898074]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>logoagenzia</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[https://silano-3r.fra1.digitaloceanspaces.com/3r/96a20c88b9d573b3f830834a54ad3f79.jpg?updated_at=2023-01-20T15:37:16.481Z]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>linkagenzia</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[www.silanosrl.it]]></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>tipocomprovendo</ATTRID>
+                <ATTRNAME><![CDATA[2190]]></ATTRNAME>
+                <ATTRVAL></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>tiporicambiaccessori</ATTRID>
+                <ATTRNAME><![CDATA[10]]></ATTRNAME>
+                <ATTRVAL></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>condizionericambi</ATTRID>
+                <ATTRNAME><![CDATA[2738]]></ATTRNAME>
+                <ATTRVAL></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>tipocomprovendo</ATTRID>
+                <ATTRNAME><![CDATA[2190]]></ATTRNAME>
+                <ATTRVAL></ATTRVAL>
+              </ATTRIBUTE>
+              <ATTRIBUTE>
+                <ATTRID>prezzo</ATTRID>
+                <ATTRNAME></ATTRNAME>
+                <ATTRVAL><![CDATA[${generatePrice(item)}]]></ATTRVAL>
+              </ATTRIBUTE>
+              
+              </ATTRIBUTELIST>
+    
+              
+              <IMAGELIST>
+    
+              ${
+                item.attributes.images && item.attributes.images.data.length
+                  ? item.attributes.images.data
+                      .map((x) => `<IMGFILE>${x.attributes.url}</IMGFILE>`)
+                      .join("")
+                  : ""
+              }
+               
+              </IMAGELIST>
+    
+              
+        </ITEM>`;
+        }
+      })
+      .join("")}
     </ITEMLIST>`;
 
   return `<?xml version="1.0" encoding="UTF-8"?>
@@ -164,8 +180,19 @@ function generateBakecaXml(items) {
 function BakecaMap() {}
 
 export async function getServerSideProps({ res }) {
-  const { data } = await getBakecaItems();
+  const { meta } = await getBakecaItems(1);
 
+  /*   const allData = [...data]; */
+  let promises = [];
+
+  for (let i = 1; i < meta.pagination.pageCount; i++) {
+    promises = [...promises, i];
+  }
+
+  const result = await Promise.all(promises.map((x) => getBakecaItems(x)));
+  const data = result.map(({ data }) => data).flat();
+
+  /* console.log(test); */
   // We generate the XML sitemap with the posts data
   const bakeca = generateBakecaXml(data);
 
--- pages/ricambi/[slug].js
+++ pages/ricambi/[slug].js
@@ -1,4 +1,48 @@
+import { getAllProducts } from "../../lib/api";
+
+import { useRef, useEffect } from "react";
+
+import Head from "next/head";
+
+import Layout from "../../components/layout/default";
+
 export default function Product() {
-  return <div>product</div>;
+  return (
+    <Layout menu={menu} footerLayout={footerLayout}>
+      <Head>
+        <title>{homepage.seo.title}</title>
+        <meta property="og:title" content={homepage.seo?.title} />
+        <meta property="og:description" content={homepage.seo?.description} />
+        <meta
+          property="og:image"
+          content={homepage.seo?.image?.data?.attributes.url}
+        />
+      </Head>
+      <div className="w-full"></div>
+    </Layout>
+  );
 }
 
+export async function getStaticPaths() {
+  const products = await getAllProducts();
+  console.log(products);
+  // Automatic generation of paths
+  const slugs = products.map(
+    (x) =>
+      new Object({
+        params: x.attributes,
+      })
+  );
+  return {
+    paths: slugs,
+    fallback: false, // can also be true or 'blocking'
+  };
+}
+
+export async function getStaticProps(context) {
+  const slug = context.params.slug;
+  console.log(slug);
+  return {
+    props: {}
+  }
+}
