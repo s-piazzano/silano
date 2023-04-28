034c488210384c971e924f2926632e5435e6c67a
Stefano <stefano@MacBook-Pro-di-Stefano.local>
2023-04-28 15:16:14 +0200

    edit whatsapp message

--- pages/ricambi/[slug].js
+++ pages/ricambi/[slug].js
@@ -121,7 +121,7 @@ Possibilità di spedizione in tutta Italia
                 className="w-52 h-12 bg-forest text-white rounded-sm uppercase mt-4 flex justify-center items-center px-4"
                 href={`https://wa.me/+393929898074?text=Ciao Silano SRL, ti contatto in merito all'annuncio ${
                   "https://www.silanosrl.it" + asPath
-                } (non modificare). Come posso procere all'acquisto?`}
+                } (non modificare). Come posso procedere all'acquisto?`}
                 passHref={true}
               >
                 <div className="flex flex-col text-center items-center">
