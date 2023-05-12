480439dd240e886ad677cdad79afc130c739800d
Stefano <stefano@MacBook-Pro-di-Stefano.local>
2023-05-12 11:57:12 +0200

    add google analitics

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="it" className="font-inter">
       <Head>
-        <link rel="icon" type="image/x-icon"  href="favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7JB23TDQ7L"
+        ></script>
+        <Script id="google-analitics">
+          {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', 'G-7JB23TDQ7L');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
