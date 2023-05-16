b3ab5003fc488482710f0ecf7950f228703812bc
Stefano <stefano@MacBook-Pro-di-Stefano.local>
2023-05-16 08:42:58 +0200

    change google analitics

--- pages/_document.js
+++ pages/_document.js
@@ -10,17 +10,18 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        <script
-          async
+
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-7JB23TDQ7L"
-        ></script>
-        <Script id="google-analitics">
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
-       
-         gtag('config', 'G-7JB23TDQ7L');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-7JB23TDQ7L');
         `}
         </Script>
       </Head>
