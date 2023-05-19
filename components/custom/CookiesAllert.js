06779f7601440da25d32a8116eaa58e56ab5dbee
Stefano <stefano@MacBook-Pro-di-Stefano.local>
2023-05-19 10:48:52 +0200

    add coockie banner

--- components/custom/CookiesAllert.js
+++ components/custom/CookiesAllert.js
@@ -24,13 +24,13 @@ export default function CookiesAllert({ className }) {
         cookiePolicy ? "hidden" : "fixed bottom-0 w-full flex justify-center"
       }
     >
-      <div className=" left-0 z-50  bg-white border-forest border  mb-4 mx-4 p-4 flex flex-col">
+      <div className="left-0 z-50  bg-white border-forest border  mb-4 mx-4 p-4 flex flex-col">
         <p>
           Noi e terze parti selezionate utilizziamo cookie o tecnologie simili
           per scopi tecnici e, con il vostro consenso, per altri scopi. Negare
           il consenso può rendere le funzionalità correlate non disponibili.
           Puoi dare, negare o revocare liberamente il tuo consenso in qualsiasi
-          momento. Usa il pulsante "Accetta" per acconsentire.
+          momento. Usa il pulsante Accetta per acconsentire.
         </p>
         <div className="flex justify-between items-end mt-2">
           <Link className="text-forest" href="/cookie-policy">
