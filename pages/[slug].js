1cce4fb7e96f90c8d061df6cfe7e16207d64c0ac
spiazzano <piazzano.stefano@gmail.com>
2023-10-06 11:10:04 +0200

    change meta

--- pages/[slug].js
+++ pages/[slug].js
@@ -22,7 +22,7 @@ export default function Page({
       <Head>
         <title>Silano SRL - {page.title}</title>
         <meta property="og:title" content={page.seo?.title} />
-        <meta name="description" content={homepage.seo?.description} />
+        <meta name="description" content={page.seo?.description} />
         <meta property="og:description" content={page.seo?.description} />
         <meta
           property="og:image"
